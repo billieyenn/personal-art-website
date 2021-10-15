446b795ec428bf22dbc190bc6bd4356c50077f9b
Jaakko Laiho <billie.yenn@gmail.com>
2021-10-15 21:54:49 +0200

    change text a bit

--- src/components/About.vue
+++ src/components/About.vue
@@ -7,9 +7,11 @@
     </p>
 
     <h1>About me</h1>
+    <!--
     <p>Billie Yenn is my pseudonymous account I have created for two reasons: Firstly, I find it very difficult to interact online with my real life profile, whereas the anonymity reduces the threshold of reaching out for me. Secondly, some NFT artwork and artists have made top dollar, and in the case that that should happen to me, I'd like to keep it separate and private in my personal life (Note: I don't inted to be a super secret shadowy coder or cyber criminal, and I do pay taxes).</p>
+    -->
     <p>
-      I discovered generative art in 2016 by using <a href="https://processing.org/">Processing</a> at a course at my education. I thought it marvellous, and to date, no other technology has been as interesting to me since. Early 2021 it occurred to me that this is a whole thing going on, and have decided to dedicate my time to sharing my creativity with the world.
+      I discovered generative art in 2016 by using <a href="https://processing.org/">Processing</a> at a course at my education. I thought it marvellous, and to date, no other technology has been as interesting to me since. Early 2021 it occurred to me that this is a whole thing going on, and have decided to dedicate time to sharing my creativity with the world.
     </p>
     <hr>
     <h1>More art to come</h1>
