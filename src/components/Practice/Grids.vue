fd5436b1ba8a034ee19992ae0e8f306bc10a01a0
Jaakko Laiho <billie.yenn@gmail.com>
2021-10-19 11:56:45 +0200

    nested grid offset seems to work

--- src/components/Practice/Grids.vue
+++ src/components/Practice/Grids.vue
@@ -14,16 +14,51 @@
 <script>
 /* eslint-enable */
 /* eslint-disable */
+import { Grid } from '../../utils.js'
 
 let sketch = (config) => {
   return function (p) {
-    p.setup = function () {
-      p.createCanvas(710, 400);
-    }
-    p.draw = function () {
-      p.background(250)
-      p.normalMaterial()
 
+    const displayRect = (x, y, scale, offsetX, offsetY) => {
+      p.stroke(0)
+      p.strokeWeight(1)
+      p.push()
+      p.translate(offsetX, offsetY)
+      p.rect(scale * x, scale * y, scale, scale)
+      p.pop()
+    }
+
+    const displayRectF = (scale, offsetX, offsetY) => {
+      return (x, y, val) => {
+        displayRect(x, y, scale, offsetX, offsetY)
+              }
+    }
+
+    const nestedGridF = (scale, offsetX, offsetY) => {
+      return (x, y, val) => {
+        if (val instanceof Grid) {
+          val.forEach(nestedGridF(scale / 3, x * scale, y * scale))
+        } else {
+          displayRect(x, y, scale, offsetX, offsetY)
+        }
+      }
+    }
+
+    p.setup = function () {
+      p.ellipseMode(p.CORNER)
+      p.createCanvas(710, 710);
+      p.background(250)
+      let scale = 300
+
+      let cols = p.floor(p.width / scale)
+      let rows = p.floor(p.height / scale)
+      let g = new Grid(rows, cols)
+      g.setVal(1, 1, new Grid(rows + 1, cols))
+      g.forEach(nestedGridF(scale, 0, 0))
+      // g.forEach(displayRectF(scale, 0, 0))
+      // g.getVal(1, 0).forEach(
+      //   displayRectF(scale/cols, 0, 0)
+      //   )
     }
   }
 }
