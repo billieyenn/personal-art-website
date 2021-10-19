9483fd3c87e220add44ef26f8bffa06658475b36
Jaakko Laiho <billie.yenn@gmail.com>
2021-10-19 14:01:22 +0200

    grid system practce

--- src/components/Practice/Grids.vue
+++ src/components/Practice/Grids.vue
@@ -19,46 +19,62 @@ import { Grid } from '../../utils.js'
 let sketch = (config) => {
   return function (p) {
 
-    const displayRect = (x, y, scale, offsetX, offsetY) => {
+    const displayRect = (x, y, width, height, offsetX, offsetY) => {
       p.stroke(0)
       p.strokeWeight(1)
       p.push()
       p.translate(offsetX, offsetY)
-      p.rect(scale * x, scale * y, scale, scale)
+      p.rect(width * x, height * y, width, height)
       p.pop()
     }
 
-    const displayRectF = (scale, offsetX, offsetY) => {
+    const displayRectF = (width, height, offsetX, offsetY) => {
       return (x, y, val) => {
-        displayRect(x, y, scale, offsetX, offsetY)
+        displayRect(x, y, width, height, offsetX, offsetY)
               }
     }
 
-    const nestedGridF = (scale, offsetX, offsetY) => {
+    const nestedGridF = (width, height, offsetX, offsetY) => {
       return (x, y, val) => {
         if (val instanceof Grid) {
-          val.forEach(nestedGridF(scale / 3, x * scale, y * scale))
+          val.forEach(nestedGridF(width / val.cols, height / val.rows, (offsetX || 0) + x * width, (offsetY || 0) + y * height))
         } else {
-          displayRect(x, y, scale, offsetX, offsetY)
+          displayRect(x, y, width, height, offsetX || 0, offsetY || 0)
         }
       }
     }
 
     p.setup = function () {
-      p.ellipseMode(p.CORNER)
-      p.createCanvas(710, 710);
+      p.createCanvas(4608 / 4, 8192 / 4);
       p.background(250)
-      let scale = 300
+      let cols = 2//p.floor(p.width / width)
+      let rows = 2//p.floor(p.height / height)
+      let width = p.width / cols//300 // of cell in grid
+      let height = p.height / rows//700 // of cell in grid
 
-      let cols = p.floor(p.width / scale)
-      let rows = p.floor(p.height / scale)
+
+
       let g = new Grid(rows, cols)
-      g.setVal(1, 1, new Grid(rows + 1, cols))
-      g.forEach(nestedGridF(scale, 0, 0))
-      // g.forEach(displayRectF(scale, 0, 0))
-      // g.getVal(1, 0).forEach(
-      //   displayRectF(scale/cols, 0, 0)
-      //   )
+      g.forEach((x, y) => {
+        if (p.random(1) < 0.75) {
+          let cellRows = p.floor(p.random(1, 4))
+          let cellCols = p.floor(p.random(1, 4))
+          let newGrid = new Grid(cellRows, cellCols)
+          g.setVal(x, y, newGrid)
+          console.log(newGrid)
+          newGrid.forEach((a, b) => {
+            if (p.random(1) < 0.75){
+              newGrid?.setVal(
+                a, 
+                b, 
+                new Grid(p.floor(p.random(1, 4)), p.floor(p.random(1, 4)))) 
+            }
+          })
+        }
+      })
+      // g.setVal(1, 1, new Grid(2, 2))
+      // g.getVal(1,1).setVal(1, 1, new Grid(8, 3))
+      g.forEach(nestedGridF(width, height, 0, 0))
     }
   }
 }
