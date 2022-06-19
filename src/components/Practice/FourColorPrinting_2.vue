5d9757a055d72bf217bba874e33c17aa58552bc8
billieyenn <billie.yenn@gmail.com>
2022-06-19 13:21:34 +0200

    little bit of refactoring and changing thing to interactive using mouse

--- src/components/Practice/FourColorPrinting_2.vue
+++ src/components/Practice/FourColorPrinting_2.vue
@@ -4,6 +4,7 @@
 <template>
   <div>
     <button @click="refresh">Redraw</button>
+    <p>Try moving your mouse across the canvas!</p>
     <div >
       <div id="canvas"></div>
     </div>
@@ -90,57 +91,27 @@ let sketch = (config) => {
       }
     }
 
-    // probably doesnt work
-    // const isInCircleF = (s, minX, minY, ellipseCenterX, ellipseCenterY, radius) => {
-    //   return (x, y) => {
-    //     return isInCircle(ellipseCenterX, ellipseCenterY, radius)((x * s + minX), (y * s + minY))
-    //   }
-    // }
-
-
-    const displayThing = (offsetX, offsetY, resize, resolution, color, symmetricity) => {
-
-      p.push()
-      p.translate(offsetX, offsetY) // put in middle of its frame of reference (in this case the screen)
-      // // start make a random shape
-      // p.angleMode(p.RADIANS)
-      // let newMachine = LineMachine(config)(p)
-      // newMachine.waves = newMachine.generator.generateRandomWaves(5, 1/(1.618), symmetricity)
-      // newMachine.tracePoints()
-      // newMachine.resize(resize)
-      // newMachine.tracePoints()
-
+    // scan the canvas and initialise the cmyk grids
+    const initCYMK = (offsetX, offsetY, resolution) => {
 
       // simulate a square box as a shape instead
       let newMachine = LineMachine(config)(p)
       // instead of filling the odd shape, fill a square
       const wholeScreen = [
 
-            p.createVector(-p.width/2, -p.height/2), 
-            p.createVector(p.width/2, -p.height/2),
-            p.createVector(p.width/2, p.height/2), 
-            p.createVector(-p.width/2, p.height/2)
+            p.createVector(-p.width + offsetX,  -p.height + offsetY), 
+            p.createVector(offsetX,             -p.height + offsetY),
+            p.createVector(offsetX,             offsetY), 
+            p.createVector(-p.width + offsetX,  offsetY)
             ]
 
       newMachine.trace = wholeScreen
 
 
-      // // use waveform input to generate shape
-      // const left = [[1730,3539],[-481,-339],[314,-372],[-194,-494],[133,-115],[53,-5],[7,-196],[112,-12],[-130,84],[74,34],[0,-80],[-72,-38],[-28,-30],[-6,8],[21,-2],[-20,-22],[13,2],[32,-43],[-27,4],[-3,-8],[6,25],[8,3],[-4,-11],[11,14],[-0,12],[16,-1],[8,-16],[12,-3],[-2,-3],[1,7],[6,0],[4,-4],[-3,-9],[2,8],[-5,4],[-3,-3],[13,-6],[9,6],[-0,0]]
-      // let vectorlist = left.map(w => p.createVector(w[0], w[1]))
-
-      // let newMachine = LineMachine(config)(p)
-      // newMachine.waves = vectorlist ? newMachine.generator.generateWavesFromVectorList(vectorlist, vectorlist.length) : newMachine.generator.generateRandomWaves(18, 1/(1.618))
-      // newMachine.tracePoints()
-      // newMachine.resize(resize)
-      // newMachine.tracePoints()
-
-
       // figure out the potential boundaries of the shape when rotated  
-      let x_max, y_max, x_min, y_min, width, height
+      let width, height
       // get an extended bounding box
-      ({ x_max,  y_max,  x_min,  y_min, width, height} = newMachine.rotationSafeBoundingBox())
-      // console.log(x_max,  y_max,  "x_min:", x_min, "y_min;",  y_min, width, height)
+      ({ width, height } = newMachine.rotationSafeBoundingBox())
 
       // scale also known as resolution. smaller number -> more computation needed
       const scale = resolution
@@ -158,18 +129,6 @@ let sketch = (config) => {
       const grids = [cyanGrid, magentaGrid, yellowGrid, blackGrid]
       const angles = [0, 15, 45, 75] // rosetta angles
  
-
-      const rgb = hexToRgb(color) // take the default color provided in DoTheThing
-      let cyan, magenta, yellow, black  
-      [cyan, magenta, yellow, black] = RGBtoCMYK(...rgb)
-      const colorsCYMK = [cyan, magenta, yellow, black] // this is a sort of backup color
-      const colorsHex = [ 
-        p.color('#00FFFF'), 
-        p.color('#FF00FF'),
-        p.color('#FFFF00'), 
-        p.color('#000000'),
-        ] // hex representation of CYMK Colors
-
       // analyse the whole current canvas to assign values for each CMYK grid
       // each rosette CMYK dot grid gets its separate iteration
       grids.forEach((grid, index) => {
@@ -177,14 +136,15 @@ let sketch = (config) => {
         grid.forEach((x, y, val) => { // iterate over each dot in the specific dot grid
 
           // index tells us which color grid we are in
-          // colorsCYMK contains contains the cymk values precomputed based on the main color in rgb given earlier
-          let color2 = colorsCYMK[index] 
+
+          let color2
           let cya, mag, yel, bla 
           let CYMKValues
           
 
           const idk = -p.width/5 // idk why this is quite right but it seems to
           const idk_y = -p.height/5 // idk why this is quite right but it seems to
+          // earlier transformations need to be undone first
           let unrotatedPoint = rotatePoint(p, offsetX, offsetY, angle, (x * scale + idk), (y * scale + idk_y))
           const colorAtPixel = p.get(unrotatedPoint[0], unrotatedPoint[1])
           let cymkResult =  RGBtoCMYK(
@@ -199,41 +159,60 @@ let sketch = (config) => {
         })
       })
 
+      return [grids, angles, newMachine]
+    }
+
+    const displayThing = (offsetX, offsetY, resolution, grids, angles, newMachine) => {
+      // scale also known as resolution. smaller number -> more computation needed
+      const scale = resolution
+     
+      //     let grids, angles, newMachine
+      // [grids, angles, newMachine] = initCYMK(p.width/2, p.height/2, scale)
+
+
+
+      // figure out the potential boundaries of the shape when rotated  
+      let x_min, y_min, width, height
+      // get an extended bounding box
+      ({  x_min,  y_min, width, height} = newMachine.rotationSafeBoundingBox())
+
+      const colorsHex = [ 
+        p.color('#00FFFF'), 
+        p.color('#FF00FF'),
+        p.color('#FFFF00'), 
+        p.color('#000000'),
+        ]
+
+  
+      p.push()
+      p.translate(offsetX, offsetY) // put in middle of its frame of reference (in this case the screen)
+
       // clear the canvas
-      p.background('white')
+      // p.background('white')
       // apply and draw the grid
+      p.fill('white')
+      p.noStroke()
+      let cursorDiameter = 75
+      let cursorRadius = cursorDiameter / 2
+      let mouseX = p.mouseX - offsetX
+      let mouseY = p.mouseY - offsetY
+      p.ellipse(mouseX, mouseY, cursorDiameter, cursorDiameter)
       grids.forEach((grid, index) => {
         let angle = angles[index] // helper variable to keep track of parameters
+
+        // // apply effect to whole screen
+        // grid.forEach(displayF(scale, angle, width/2, colorsHex[index]),
+        //   isInPolyF(rotatePoints(p, newMachine.trace, angle, x_min, y_min, width, height), scale, x_min, y_min)
+        //   )
+
+        // apply effect close to mouse
         grid.forEach(displayF(scale, angle, width/2, colorsHex[index]),
-          isInPolyF(rotatePoints(p, newMachine.trace, angle, x_min, y_min, width, height), scale, x_min, y_min)
+          isNearPointF(rotatePoints(p, [p.createVector(mouseX, mouseY)], angle, x_min, y_min, width, height)[0], scale, x_min, y_min, cursorRadius)
           )
+
       })
 
       p.pop()
-
-      // // frame and debugging
-      // p.push()
-      // p.translate(offsetX - x_min - width / 2, offsetY - y_min - height / 2)
-
-      // // debugging shape outline
-      //  // drawShapeOutline(newMachine.trace, p.color(colors.mineShaft))
-      // // drawShapeOutline(newMachine.trace, p.color(...rgb))//mineShaft))
-
-      // // newMachine.fill = false
-      // // newMachine.color = p.color(rgb)
-      // // newMachine.display(30, 1)
-
-      // // // debugging bounding box
-      // // p.noFill()
-      // // p.rect(x_min, y_min, width, height)
-      
-      // // const info = newMachine.traceInfo()
-      // // const margin = 20
-      // // p.strokeWeight(margin - 10)
-      // // p.noFill()
-      // // p.rect(info.minX - margin/2, info.minY - margin/2, info.maxX -  info.minX + margin,  info.maxY -  info.minY + margin)
-
-      // p.pop()
     }
 
 
@@ -242,7 +221,6 @@ let sketch = (config) => {
       let strokeWeight = p.sqrt(p.min(width,height))
       p.strokeWeight(strokeWeight)
       p.noFill()
-      // p.rect(width * x + strokeWeight, height * y + strokeWeight, width - strokeWeight*2, height - strokeWeight*2, p.sqrt(p.min(width,height)))
       p.rect(0, 0, width , height, 30)
     }
 
@@ -254,10 +232,17 @@ let sketch = (config) => {
     }
 
     // setup
+    const size = 1000 // canvas size
+    const scale = 2 // big number means faster render time
+
+    let offsetX
+    let offsetY
+    const color = colors.bigStone/*colors.springWood*//*colors.goblin*//*randomColor(unnamedColorScheme)*/
+    let grids2, angles2, newMachine2
+
     p.setup = function () {
       // prepare canvas
       // p.createCanvas(4608 / 8, 8192 / 8); // handy tall frame
-      const size = 500
       p.createCanvas(size, size);
       p.background(p.color(colors.springWood))
       noisyBackground(p, hexToRgb(colors.springWood), 7)
@@ -269,18 +254,43 @@ let sketch = (config) => {
       // read config
       const width = p.width
       const height = p.height
-      const resize = p.min(width, height) / p.height * 1.3//(width > height ? p.height : p.width)
-      const scale = 2 //big number means faster render time
-      const color = colors.bigStone/*colors.springWood*//*colors.goblin*//*randomColor(unnamedColorScheme)*/
-      const symmetricity = p.random(1) < 0.2 //symmetricity is true once in five
+
+      offsetX = 0.5 * width
+      offsetY = 0.5 * height
+
+      // some weird syntax bug prevents compilation due to assigning value left hand side, hence this dumb solution
+      let grids, angles, newMachine
+      [grids, angles, newMachine] = initCYMK(offsetX, offsetY, scale) // analyse image and convert to CYMK dot grids
+      grids2 = grids
+      angles2 = angles
+      newMachine2 = newMachine
+      p.background(p.color(colors.springWood)) // clear out image
 
       // do the main thing
-      displayThing(0.5 * width, 0.5 * height, resize, scale, color, symmetricity)
+      displayThing(offsetX, offsetY, scale, grids2, angles2, newMachine2)
+
 
       displayBorder(0, 0, width, height, color)
 
       // again introduce noise to simulate effect of photograph
       noiseEverywhere(p, 3)
+    }
+
+    p.draw = function() {
+      p.background(p.color(colors.springWood))
+
+      // img.resize(size, 0)
+      // p.image(img,0,0)
+
+      // read config
+      const width = p.width
+      const height = p.height
+
+      // do the main thing
+      displayThing(offsetX, offsetY, scale, grids2, angles2, newMachine2)
+      displayBorder(0, 0, width, height, color)
+
+
     }
   }
 }
