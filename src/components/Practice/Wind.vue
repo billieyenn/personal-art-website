51ae1636da50be4937d238c7eae6ec1801ec2365
jaakko <jaakkolaiho94@gmail.com>
2025-01-05 17:43:29 +0100

    Display air pressure flow field

--- src/components/Practice/Wind.vue
+++ src/components/Practice/Wind.vue
@@ -34,25 +34,30 @@ let sketch = (config) => {
             // the flow field keeps track of local gravity
             flowField = new Grid(rows, cols)
             flowField.forEach((x, y, val) => {
-                flowField.setVal(x, y, p.createVector(0, 0))
+                // create a vector of random magnitude
+                let r = p.noise(x, y) // generate a random value between 0 and 1
+                let v = p.createVector(r, 0)
+
+                // fill flowfield with vector
+                flowField.setVal(x, y, v)
             })
         }
 
         p.draw = function () {
             p.background(randomBGColor)
             
-            // show flowfield outline
-            p.stroke(0)
-            p.noFill()
-            p.rectMode(p.CENTER)
+            // Display the flowfield encoding pressure for debugging
             flowField.forEach((x, y, val) => {
-                const x_ff = (x + 0.5) * scale // the 0.5 offset centers the dot
-                const y_ff = (y + 0.5) * scale
+                // draw the grid
                 p.strokeWeight(0.5)
+                p.noFill()
+                p.stroke(0)
+                p.rectMode(p.CENTER)
                 p.rect((x+0.5)*scale, (y+0.5)*scale, scale, scale)
-                p.line(x_ff, y_ff, x_ff + val.x*scale, y_ff + val.y*scale)
-                p.strokeWeight(2)
-                p.point(x_ff + val.x*scale, y_ff + val.y*scale)
+
+                // draw local 'pressure'
+                p.fill(0)
+                p.circle((x+0.5)*scale, (y+0.5)*scale, val.x /* .x of vector encodes pressure */* scale) 
             })
         }
     }
