44f602c0bd9daa6a18a707105abecae92daf3c4e
jaakko <jaakkolaiho94@gmail.com>
2025-01-05 21:32:29 +0100

    Calculate air pressure with averages instead

--- src/components/Practice/Wind.vue
+++ src/components/Practice/Wind.vue
@@ -22,7 +22,6 @@ let cols
 const randomBGColor = randomColor(colors)
 const scale = 50
 let airPressureFlowField
-let windFlowField
 
 let sketch = (config) => {
     return function (p) {
@@ -41,20 +40,6 @@ let sketch = (config) => {
 
                 // fill airPressureFlowField with vector
                 airPressureFlowField.setVal(x, y, v)
-            })
-
-            // the flow field keeps track of wind direction
-            windFlowField = new Grid(rows, cols)
-            windFlowField.forEach((x, y, val) => {
-                // todo: initialise wind in direction of lowest pressure
-                let r = p.noise(x, y) / 2 // generate a random value between 0 and 0.5
-                let v = p.createVector(r, 0)
-                let angle = p.noise(x, y) * p.TWO_PI  // generate a random value between 0 and 2 PI
-                p.angleMode(p.RADIANS)
-                v.rotate(angle)
-
-                // fill windFlowField with vector
-                windFlowField.setVal(x, y, v)
             })
         }
 
@@ -78,55 +63,40 @@ let sketch = (config) => {
             })
 
             // Display the windFlowField encoding wind for debugging
-            windFlowField.forEach((x, y, val) => {
+            airPressureFlowField.forEach((x, y, val) => {
                 // draw local 'wind'
                 p.stroke(255)
-                p.line((x+0.5)*scale, (y+0.5)*scale, (x+0.5)*scale + val.x * scale, (y+0.5)*scale + val.y * scale) 
+                p.line((x+0.5)*scale, (y+0.5)*scale, (x+0.5)*scale, (y+0.5)*scale + val.y * scale / 2)
             })
             
-            // Compute average directional air pressure at each location
-            windFlowField.forEach((x, y, val) => {
-                // get each neighbouring air pressure
-                // 1 2 3
-                // 4 5 6
-                // 7 8 9
-                const p1 = airPressureFlowField.getVal(x - 1,   y - 1) ?? p.createVector(0, 0)
-                const p2 = airPressureFlowField.getVal(x,       y - 1) ?? p.createVector(0, 0)
-                const p3 = airPressureFlowField.getVal(x + 1,   y - 1) ?? p.createVector(0, 0)
-                const p4 = airPressureFlowField.getVal(x - 1,   y) ?? p.createVector(0, 0)
-                const p5 = airPressureFlowField.getVal(x,       y) ?? p.createVector(0, 0)
-                const p6 = airPressureFlowField.getVal(x + 1,   y) ?? p.createVector(0, 0)
-                const p7 = airPressureFlowField.getVal(x - 1,   y + 1) ?? p.createVector(0, 0)
-                const p8 = airPressureFlowField.getVal(x,       y + 1) ?? p.createVector(0, 0)
-                const p9 = airPressureFlowField.getVal(x + 1,   y + 1) ?? p.createVector(0, 0)
+            // Calculate effects of air pressure on wind
+            airPressureFlowField.forEach((x, y, val) => {
+                const cluster = [airPressureFlowField.getVal(x - 1,   y - 1),
+                    airPressureFlowField.getVal(x,       y - 1),
+                    airPressureFlowField.getVal(x + 1,   y - 1),
+                    airPressureFlowField.getVal(x - 1,   y),
+                    airPressureFlowField.getVal(x,       y),
+                    airPressureFlowField.getVal(x + 1,   y),
+                    airPressureFlowField.getVal(x - 1,   y + 1),
+                    airPressureFlowField.getVal(x,       y + 1),
+                    airPressureFlowField.getVal(x + 1,   y + 1)
+                ]
+                const nonNullCluster = cluster
+                    .filter(value => value !== undefined)
+                    
+                const clusterSum = nonNullCluster
+                    .reduce((acc, value) => acc + value.x, 0)
+                const clusterAverage = clusterSum / nonNullCluster.length
 
-                // compute the gradient in a rudimentary way
-                p.angleMode(p.RADIANS)
-                let sumPressure = p.createVector(0, 0)
-                sumPressure.add(p1.copy().rotate(p.PI + p.QUARTER_PI))
-                sumPressure.add(p2.copy().rotate(p.PI + p.HALF_PI))
-                sumPressure.add(p3.copy().rotate(-p.QUARTER_PI))
-                sumPressure.add(p4.copy().rotate(p.PI))
-                // sumPressure.add(p5.copy().rotate(-p.QUARTER_PI)) // todo> pressure at self affects differently, consider this carefully
-                sumPressure.add(p6.copy().rotate(0))
-                sumPressure.add(p7.copy().rotate(p.HALF_PI + p.QUARTER_PI))
-                sumPressure.add(p8.copy().rotate(p.HALF_PI))
-                sumPressure.add(p9.copy().rotate(p.QUARTER_PI))
-
-                sumPressure.rotate(p.PI) /* wind of course blows towards low pressure*/
-
-                // wind magnitude is differential between local average and this
-                sumPressure.setMag(p.abs( p5.mag() - sumPressure.mag() )) 
-                // todo: consider how wind should not blow out of low pressure area.
-                // after all, if there is no air, how can there be wind?
-
-                // Apply force to wind
-                windFlowField.setVal(x, y, sumPressure)
+                console.log(clusterAverage)
+                
+                const newVal = airPressureFlowField.getVal(x, y)
+                newVal.y = clusterAverage
             })
 
-            // Wind affects air pressure
-            windFlowField.forEach((x, y, val) => {
-                // todo
+            // apply wind
+            airPressureFlowField.forEach((x, y, val) => {
+                // todoi
             })
 
             // todo: consider edges. now wind 'falls' out
